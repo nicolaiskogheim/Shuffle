abb5ce4ca96bdfcbf2a157035603f61238951bf6
jonas <jonas.rudlang@gmail.com>
2020-10-24 16:54:20 +0200

    Enable remaining enpoints for Authentication Required tests
    A local database is now required for the tests to run.

--- backend/go-app/main_test.go
+++ backend/go-app/main_test.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"cloud.google.com/go/datastore"
+	"google.golang.org/api/option"
+
+	"google.golang.org/grpc"
 )
 
 type endpoint struct {
@@ -21,26 +28,24 @@ func TestAuthenticationRequired(t *testing.T) {
 		{handler: handleGetOutlookFolders, path: "/functions/outlook/getFolders", method: "GET"},
 		{handler: handleApiGeneration, path: "/api/v1/users/generateapikey", method: "GET"},
 		// handleLogin generates nil pointer exception
-		// {handler: handleLogin, path: "/api/v1/users/login", method: "POST"}, // prob not this one
+		{handler: handleLogin, path: "/api/v1/users/login", method: "POST"}, // prob not this one
 		// handleRegister generates nil pointer exception
-		// {handler: handleRegister, path: "/api/v1/users/register", method: "POST"},
-		// checkAdminLogin generates nil pointer exception
-		// {handler: checkAdminLogin, path: "/api/v1/users/checkusers", method: "GET"},
+		{handler: handleRegister, path: "/api/v1/users/register", method: "POST"},
 		{handler: handleGetUsers, path: "/api/v1/users/getusers", method: "GET"},
 		{handler: handleInfo, path: "/api/v1/users/getinfo", method: "GET"},
 		{handler: handleSettings, path: "/api/v1/users/getsettings", method: "GET"},
 		{handler: handleUpdateUser, path: "/api/v1/users/updateuser", method: "PUT"},
 		{handler: deleteUser, path: "/api/v1/users/123", method: "DELETE"},
 		// handlePasswordChange generates nil pointer exception
-		// {handler: handlePasswordChange, path: "/api/v1/users/passwordchange", method: "POST"},
+		{handler: handlePasswordChange, path: "/api/v1/users/passwordchange", method: "POST"},
 		{handler: handleGetUsers, path: "/api/v1/users", method: "GET"},
 		{handler: handleGetEnvironments, path: "/api/v1/getenvironments", method: "GET"},
 		{handler: handleSetEnvironments, path: "/api/v1/setenvironments", method: "PUT"},
 
 		// handleWorkflowQueue generates nil pointer exception
-		// {handler: handleWorkflowQueue, path: "/api/v1/streams", method: "POST"},
+		{handler: handleWorkflowQueue, path: "/api/v1/streams", method: "POST"},
 		// handleGetStreamResults generates nil pointer exception
-		// {handler: handleGetStreamResults, path: "/api/v1/streams/results", method: "POST"},
+		{handler: handleGetStreamResults, path: "/api/v1/streams/results", method: "POST"},
 
 		{handler: handleAppHotloadRequest, path: "/api/v1/apps/run_hotload", method: "GET"},
 		{handler: loadSpecificApps, path: "/api/v1/apps/get_existing", method: "POST"},
@@ -70,9 +75,9 @@ func TestAuthenticationRequired(t *testing.T) {
 		{handler: scheduleWorkflow, path: "/api/v1/workflows/123/schedule", method: "POST"},
 		{handler: stopSchedule, path: "/api/v1/workflows/123/schedule/abc", method: "DELETE"},
 		// createOutlookSub generates nil pointer exception
-		// {handler: createOutlookSub, path: "/api/v1/workflows/123/outlook", method: "POST"},
+		{handler: createOutlookSub, path: "/api/v1/workflows/123/outlook", method: "POST"},
 		// handleDeleteOutlookSub generates nil pointer exception
-		// {handler: handleDeleteOutlookSub, path: "/api/v1/workflows/123/outlook/abc", method: "DELETE"},
+		{handler: handleDeleteOutlookSub, path: "/api/v1/workflows/123/outlook/abc", method: "DELETE"},
 		{handler: getWorkflowExecutions, path: "/api/v1/workflows/123/executions", method: "GET"},
 		{handler: abortExecution, path: "/api/v1/workflows/123/executions/abc/abort", method: "GET"},
 		{handler: getSpecificWorkflow, path: "/api/v1/workflows/123", method: "GET"},
@@ -95,8 +100,25 @@ func TestAuthenticationRequired(t *testing.T) {
 
 		{handler: cleanupExecutions, path: "/api/v1/execution_cleanup", method: "GET"},
 	}
+
+	var err error
+	ctx := context.Background()
+
+	// Most handlers requires database access in order to not crash or cause
+	// nil pointer issues.
+	// To start a local database instance, run:
+	//   docker-compose up database
+	// To let the tests know about the database, run:
+	//   DATASTORE_EMULATOR_HOST=0.0.0.0:8000 go test
+	dbclient, err = datastore.NewClient(ctx, gceProject, option.WithGRPCDialOption(grpc.WithNoProxy()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dummyBody := bytes.NewBufferString("dummy")
+
 	for _, e := range handlers {
-		req, err := http.NewRequest(e.method, e.path, nil)
+		req, err := http.NewRequest(e.method, e.path, dummyBody)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -115,6 +137,8 @@ func TestAuthenticationRequired(t *testing.T) {
 
 func TestAuthenticationNotRequired(t *testing.T) {
 	handlers := []endpoint{
+		// checkAdminLogin returns 200 OK when not logged in
+		{handler: checkAdminLogin, path: "/api/v1/users/checkusers", method: "GET"},
 		// handleLogout returns 200 OK when not logged in
 		{handler: handleLogout, path: "/api/v1/users/logout", method: "POST"},
 		// getDocsList returns 200 OK when not logged in
